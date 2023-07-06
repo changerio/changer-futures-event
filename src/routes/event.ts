acdb41700b4186f798b030d020a0484415a688dd
sunpuyo <sunpuyo@gmail.com>
2023-07-06 13:03:51 +0900

    Limit external calls to api with heavy load

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,15 +1,34 @@
 import { Request, Response, Router } from "express";
 
-import { getRankingOfPnl, getRankingOfPnlRealTime, getRankingOfTrader, getRankingOfTradingVolume, getRankingOfTradingVolumeRealTime, setMainnetOpenEvent, upsertMainnetOpenEvent } from "../services/eventService";
+import {
+  getRankingOfPnl,
+  getRankingOfPnlRealTime,
+  getRankingOfTrader,
+  getRankingOfTradingVolume,
+  getRankingOfTradingVolumeRealTime,
+  setMainnetOpenEvent,
+  upsertMainnetOpenEvent,
+} from "../services/eventService";
 
 /**
  * @swagger
- * tags: 
+ * tags:
  *   name: Event
  *   description: 이벤트 조회용 API
  */
 const eventRouter = Router();
 
+function onlyFromLocal(req: Request) {
+  const clientIp =
+    req.headers["x-forwarded-for"] || req.socket?.remoteAddress || null;
+  var host = req.get("host");
+  return (
+    clientIp === "127.0.0.1" ||
+    clientIp === "::ffff:127.0.0.1" ||
+    clientIp === "::1" ||
+    host?.indexOf("localhost") !== -1
+  );
+}
 /**
  * @swagger
  * /event/mainnet-open/tv/realtime/:
@@ -24,11 +43,18 @@ const eventRouter = Router();
  *             schema:
  *              type: object
  */
-eventRouter.get("/mainnet-open/tv/realtime", async (req: Request, res: Response) => {
-    const ret = await getRankingOfTradingVolumeRealTime();
+eventRouter.get(
+  "/mainnet-open/tv/realtime",
+  async (req: Request, res: Response) => {
+    if (onlyFromLocal(req)) {
+      const ret = await getRankingOfTradingVolumeRealTime();
 
-    return res.status(200).send(ret);
-});
+      return res.status(200).send(ret);
+    } else {
+      return res.status(401).send();
+    }
+  }
+);
 
 /**
  * @swagger
@@ -44,12 +70,18 @@ eventRouter.get("/mainnet-open/tv/realtime", async (req: Request, res: Response)
  *             schema:
  *              type: object
  */
-eventRouter.get("/mainnet-open/pnl/realtime", async (req: Request, res: Response) => {
-    const ret = await getRankingOfPnlRealTime();
+eventRouter.get(
+  "/mainnet-open/pnl/realtime",
+  async (req: Request, res: Response) => {
+    if (onlyFromLocal(req)) {
+      const ret = await getRankingOfPnlRealTime();
 
-    return res.status(200).send(ret);
-});
-
+      return res.status(200).send(ret);
+    } else {
+      return res.status(401).send();
+    }
+  }
+);
 
 /**
  * @swagger
@@ -66,9 +98,9 @@ eventRouter.get("/mainnet-open/pnl/realtime", async (req: Request, res: Response
  *              type: object
  */
 eventRouter.get("/mainnet-open/tv", async (req: Request, res: Response) => {
-    const ret = await getRankingOfTradingVolume();
+  const ret = await getRankingOfTradingVolume();
 
-    return res.status(200).send(ret);
+  return res.status(200).send(ret);
 });
 
 /**
@@ -86,9 +118,9 @@ eventRouter.get("/mainnet-open/tv", async (req: Request, res: Response) => {
  *              type: object
  */
 eventRouter.get("/mainnet-open/pnl", async (req: Request, res: Response) => {
-    const ret = await getRankingOfPnl();
+  const ret = await getRankingOfPnl();
 
-    return res.status(200).send(ret);
+  return res.status(200).send(ret);
 });
 
 /**
@@ -114,9 +146,9 @@ eventRouter.get("/mainnet-open/pnl", async (req: Request, res: Response) => {
  *              type: object
  */
 eventRouter.get("/mainnet-open/user", async (req: Request, res: Response) => {
-    const address: string = req.query.address as string;
-    const ret = await getRankingOfTrader(address.toLowerCase());
-    return res.status(200).send(ret);
+  const address: string = req.query.address as string;
+  const ret = await getRankingOfTrader(address.toLowerCase());
+  return res.status(200).send(ret);
 });
 
 /**
@@ -134,8 +166,12 @@ eventRouter.get("/mainnet-open/user", async (req: Request, res: Response) => {
  *              type: object
  */
 eventRouter.post("/mainnet-open/", async (req: Request, res: Response) => {
+  if (onlyFromLocal(req)) {
     const ret = await setMainnetOpenEvent();
     return res.status(200).send(ret);
+  } else {
+    return res.status(401).send();
+  }
 });
 
 /**
@@ -153,8 +189,12 @@ eventRouter.post("/mainnet-open/", async (req: Request, res: Response) => {
  *              type: object
  */
 eventRouter.put("/mainnet-open/", async (req: Request, res: Response) => {
+  if (onlyFromLocal(req)) {
     const ret = await upsertMainnetOpenEvent();
     return res.status(200).send(ret);
+  } else {
+    return res.status(401).send();
+  }
 });
 
 export { eventRouter };
