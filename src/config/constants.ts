387f98b526979912e07da3e3de6f362a611e1030
DaYoung Choi <happyring0@naver.com>
2023-09-14 15:27:18 +0900

    set zksyncEra subgraphs

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,6 +6,6 @@ export enum CHAIN_IDS {
 
 export enum SUBGRAPHS {
   hardhat = 'http://127.0.0.1:8000/subgraphs/name/changerio/gambit',
-  zksyncEra = 'https://api.studio.thegraph.com/proxy/47348/gambit-zksync-alpha/version/latest', //'http://3.35.234.124:8000/subgraphs/name/changerio/gambit',
+  zksyncEra = 'https://api.studio.thegraph.com/proxy/47348/gambit-zksync/version/latest', //'http://3.35.234.124:8000/subgraphs/name/changerio/gambit',
   zksyncEraGoerli = 'https://api.studio.thegraph.com/proxy/47348/gambit-zksync-alpha/version/latest',//'http://3.35.234.124:8000/subgraphs/name/changerio/gambit',
 }
