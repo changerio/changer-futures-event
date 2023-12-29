6b27ae7b7538c68cf4dc4d23bfc18810f895723b
DaYoung Choi <happyring0@naver.com>
2023-12-29 15:25:16 +0900

    stop execution of event batch after event period

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -6,58 +6,66 @@ import { setMainnetOpenEvent, upsertMainnetOpenEvent } from "../services/eventSe
 import { excuteDashboardQuery, setAPR } from "../services/duneService";
 
 async function updateMainnetOpenEventRanking() {
-  try {
-    logger.info(`[UpdateMainnetOpenEventRanking] ${new Date()}`);
-    await upsertMainnetOpenEvent();
-  } catch (e) {
-    logger.error(`[UpdateMainnetOpenEventRanking] Fail`);
-    logger.error(e);
-    setTimeout(updateMainnetOpenEventRanking, 1000); // 1s
-  }
+    try {
+        logger.info(`[UpdateMainnetOpenEventRanking] ${new Date()}`);
+        await upsertMainnetOpenEvent();
+    } catch (e) {
+        logger.error(`[UpdateMainnetOpenEventRanking] Fail`);
+        logger.error(e);
+        setTimeout(updateMainnetOpenEventRanking, 1000); // 1s
+    }
 }
 
 async function updateMidnightPairPrice() {
-  try {
-    logger.info(`[UpdateMidnightPairPrice] ${new Date()}`);
-    await setMidnightPairPrice();
-  } catch (e) {
-    logger.error(`[UpdateMidnightPairPrice] Fail`);
-    logger.error(e);
-    setTimeout(updateMidnightPairPrice, 1000); // 1s
-  }
+    try {
+        logger.info(`[UpdateMidnightPairPrice] ${new Date()}`);
+        await setMidnightPairPrice();
+    } catch (e) {
+        logger.error(`[UpdateMidnightPairPrice] Fail`);
+        logger.error(e);
+        setTimeout(updateMidnightPairPrice, 1000); // 1s
+    }
 }
 
 async function excuteDuneDashboardQuery() {
-  try {
-    logger.info(`[ExcuteDuneDashboardQuery] ${new Date()}`);
-    await excuteDashboardQuery();
-  } catch (e) {
-    logger.error(`[ExcuteDuneDashboardQuery] Fail`);
-    logger.error(e);
-  }
+    try {
+        logger.info(`[ExcuteDuneDashboardQuery] ${new Date()}`);
+        await excuteDashboardQuery();
+    } catch (e) {
+        logger.error(`[ExcuteDuneDashboardQuery] Fail`);
+        logger.error(e);
+    }
 }
 
+const EVENT_END_TIME = 1704499210000; // 2024년 1월 6일 0시 0분 0초 (GMT) + 10s
+
 export const loadWorker = () => {
-  // Called once at startup
-  updateMainnetOpenEventRanking();
-  updateMidnightPairPrice();
-  setAPR();
-
-  // every 10m
-  schedule.scheduleJob("*/10 * * * *", () => {
+    // Called once at startup
     updateMainnetOpenEventRanking();
-  });
-
-  // 00:00:01
-  schedule.scheduleJob("1 0 0 * * *", () => {
-    excuteDuneDashboardQuery();
     updateMidnightPairPrice();
-    setMainnetOpenEvent();
-  });
-
-  // 00:00:30
-  schedule.scheduleJob("30 0 0 * * *", () => {
     setAPR();
-  });
+
+    // every 10m
+    schedule.scheduleJob("*/10 * * * *", () => {
+        const currentTimestamp = (new Date()).getTime();
+        if (currentTimestamp < EVENT_END_TIME) {
+            updateMainnetOpenEventRanking();
+        }
+    });
+
+    // daily - 00:00:01
+    schedule.scheduleJob("1 0 0 * * *", () => {
+        excuteDuneDashboardQuery();
+        updateMidnightPairPrice();
+        const currentTime = (new Date()).getTime();
+        if (currentTime < EVENT_END_TIME) {
+            setMainnetOpenEvent();
+        }
+    });
+
+    // daily - 00:00:30
+    schedule.scheduleJob("30 0 0 * * *", () => {
+        setAPR();
+    });
 };
 
